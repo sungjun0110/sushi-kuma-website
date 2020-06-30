78981f6595632503c6c5ec668e44e45919602df1
sungjun0110 <55672234+sungjun0110@users.noreply.github.com>
2020-06-29 17:01:11 -0700

    First commit
    working on main nav bar

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php wp_head();?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta http-http-equiv="X-YA-Compatible" content="ie=edge">
+    <meta name="keywords" content="Sushi Kuma">
+    <meta name="description" content="Quality Sushi">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  </head>
+
+  <body <?php body_class();?>>
+
+    <header>
+
+      <div class="header">
+
+        <div class="header-left">
+          <a href="" class="logo"><img width="100px" src="asset/images/kuma-logo.png"></a>
+        </div>
+
+        <div class="menu-toggle"></div>
+
+        <div class="header-right">
+          <nav class="main-nav">
+            <?php
+              $args = array(
+                'theme_location' => 'top-menu'
+              );
+            ?>
+            <?php wp_nav_menu( $args ) ?>
+          </nav>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+
+    </header>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php get_header();?>
+<body>
+  <div class="slidershow">
+    <div class="slides">
+      <input type="radio" name="r" id="r1" checked>
+      <input type="radio" name="r" id="r2">
+      <div class="slide s1">
+        <a href="" class="slide1"><img src="asset/images/slide1.png" alt="photo" class="responsive"></a>
+      </div>
+      <div class="slide">
+        <a herf="" class="slide2"><img src="asset/images/slide2.png" alt="photo" class="responsive"></a>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="middle-container">
+    <div class="locations">
+      <h1>Location</h1>
+    </div>
+  </div>
+
+</body>
