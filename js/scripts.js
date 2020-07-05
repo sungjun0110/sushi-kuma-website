1c7fb83f55d1e96c7a99125817a4e0cc3c797cda
sungjun0110 <55672234+sungjun0110@users.noreply.github.com>
2020-07-05 02:40:31 -0700

    Menu layout modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,100 @@
 $(document).ready(function() {
+
   $('.menu-toggle').click(function(){
     $('.menu-toggle').toggleClass('active');
     $('nav').toggleClass('active');
     $('.header-right').toggleClass('slide-down');
     $('.main-nav').toggleClass('slide-down');
-  })
-})
+  });
+
+  $('#appetizer').click(function() {
+    if ($('#appetizer-content').is(':visible')){
+      $('#appetizer-content').slideUp("slow");
+    }
+    else{
+      $('#appetizer-content').slideDown("slow");
+      $('#appetizer-content').attr("color", "red");
+    }
+  });
+
+  $('#nigiri').click(function() {
+    if ($('#nigiri-content').is(':visible')){
+      $('#nigiri-content').slideUp("slow");
+    }
+    else{
+      $('#nigiri-content').slideDown("slow");
+    }
+  });
+
+  $('#street-food').click(function() {
+    if ($('#street-food-content').is(':visible')){
+      $('#street-food-content').slideUp("slow");
+    }
+    else{
+      $('#street-food-content').slideDown("slow");
+    }
+  });
+
+  $('#entree').click(function() {
+    if ($('#entree-content').is(':visible')){
+      $('#entree-content').slideUp("slow");
+    }
+    else{
+      $('#entree-content').slideDown("slow");
+    }
+  });
+
+  $('#kids').click(function() {
+    if ($('#kids-content').is(':visible')){
+      $('#kids-content').slideUp("slow");
+    }
+    else{
+      $('#kids-content').slideDown("slow");
+    }
+  });
+
+  $('#rolls').click(function() {
+    if ($('#rolls-content').is(':visible')){
+      $('#rolls-content').slideUp("slow");
+    }
+    else{
+      $('#rolls-content').slideDown("slow");
+    }
+  });
+
+  $('#ksr').click(function() {
+    if ($('#ksr-content').is(':visible')){
+      $('#ksr-content').slideUp("slow");
+    }
+    else{
+      $('#ksr-content').slideDown("slow");
+    }
+  });
+
+  $('#sides').click(function() {
+    if ($('#sides-content').is(':visible')){
+      $('#sides-content').slideUp("slow");
+    }
+    else{
+      $('#sides-content').slideDown("slow");
+    }
+  });
+
+  $('#alcohol').click(function() {
+    if ($('#alcohol-content').is(':visible')){
+      $('#alcohol-content').slideUp("slow");
+    }
+    else{
+      $('#alcohol-content').slideDown("slow");
+    }
+  });
+
+  $('#bnd').click(function() {
+    if ($('#bnd-content').is(':visible')){
+      $('#bnd-content').slideUp("slow");
+    }
+    else{
+      $('#bnd-content').slideDown("slow");
+    }
+  });
+});
--- page-roseville-menu.php
+++ page-roseville-menu.php
@@ -1,27 +1,275 @@
 <?php get_header(); ?>
 
-<div class="menu-categories">
-  <button type="button" class="selected">Appetizer</button>
-  <button type="button">Nigiri</button>
-</div>
-
 <div class="menu-container">
-    <?php
 
-      query_posts("category_name=rosevillemenu&posts_per_page=100");
+  <div class="menu-category" id="appetizer">Appetizer</div><hr/>
+    <div class="menu-content" id="appetizer-content">
 
-      if (have_posts()) : while(have_posts()) : the_post(); ?>
+      <div class="item-title">Salad</div>
 
-      <h1><?php the_title(); ?></h1>
+      <div class="items">
+        <div class="item">
+          <h4>Edamame</h4>
+          <p>boiled and salted soybeans<br>5.50</p>
+        </div>
+        <div class="item">
+          <h4>Spicy Edamame</h4>
+          <p>pan-fried soybeans with Japanese spice mix<br>6.50</p>
+        </div>
+        <div class="item">
+          <h4>Green Salad</h4>
+          <p>served with house dressing sauce<br>4.95</p>
+        </div>
+        <div class="item">
+          <h4>Seaweed Salad</h4>
+          <p>seaweed marinated with sesame oil<br>5.95</p>
+        </div>
+        <div class="item">
+          <h4>Veggie Sunomono</h4>
+          <p>cucumber in vinegar sauce<br>5.50</p>
+        </div>
+        <div class="item">
+          <h4>Seafood Sunomono </h4>
+          <p>cucumber with assorted seafood in vinegar sauce<br>7.95</p>
+        </div>
+        <div class="item">
+          <h4>Hawaiian Poke</h4>
+          <p>ahi tuna, cucumber, onion with ponzu, sesame oil, spicy sauce & tobiko<br>11.95</p>
+        </div>
+      </div>
 
-      <?php if(has_post_thumbnail()):?>
-        <img src="<?php the_post_thumbnail_url('smallest');?>" class="img-fluid">
-      <?php endif;?>
 
-      <p><?php the_content(); ?></p>
+      <div class="item-title">Grilled</div>
 
-      <?php echo '<hr/>'; ?>
+      <div class="items">
+        <div class="item">
+          <h4>Baked Green Mussel</h4>
+          <p>baked on half shell & spicy sauce - 5 pcs<br>7.50</p>
+        </div>
+        <div class="item">
+          <h4>BBQ Albacore</h4>
+          <p>grilled rare white tuna with special spicy BBQ sauce - 6 pcs<br>14.95</p>
+        </div>
+        <div class="item">
+          <h4>Sea Steak</h4>
+          <p>grilled ahi tuna with ponzu & special sauce - 8 pcs<br>14.95</p>
+        </div>
+        <div class="item">
+          <h4>Chicken Teriyaki</h4>
+          <p>grilled chicken with teriyaki sauce<br>9.95</p>
+        </div>
+        <div class="item">
+          <h4>Hamachi Kama</h4>
+          <p>grilled yellow tail cheek bone with ponzu sauce<br>10.50</p>
+        </div>
+      </div>
 
-    <?php endwhile; endif; ?>
+
+      <div class="item-title">Deep-Fried</div>
+
+      <div class="items">
+        <div class="item">
+          <h4>Agedashi Tofu</h4>
+          <p>deep-fried tofu served with dried tuna, ponzu sauce<br>7.95</p>
+        </div>
+        <div class="item">
+          <h4>Calamari Tempura</h4>
+          <p>breaded deep-fried calamari ring<br>8.50</p>
+        </div>
+        <div class="item">
+          <h4>Croquette</h4>
+          <p>deep-fried potato & vegetables – 3pcs<br>6.50</p>
+        </div>
+        <div class="item">
+          <h4>Creamy Jalapeno </h4>
+          <p>deep-fried jalapeno, cream cheese, spicy tuna special sauce<br>6.95</p>
+        </div>
+        <div class="item">
+          <h4>Gyoza</h4>
+          <p>Deep-fried or steamed chicken and vegetable pot-stickers - 7 pcs<br>7.50</p>
+        </div>
+        <div class="item">
+          <h4>Vegetable Tempura</h4>
+          <p>assorted seasonal vegetables<br>7.50</p>
+        </div>
+        <div class="item">
+          <h4>Shrimp Tempura</h4>
+          <p>deep-fried shrimp - 4 pcs<br>9.95</p>
+        </div>
+        <div class="item">
+          <h4>Mixed Tempura</h4>
+          <p>shrimp - 2 pcs, vegetables - 5 pcs<br>8.95</p>
+        </div>
+        <div class="item">
+          <h4>Honey Spice Chicken</h4>
+          <p>pan fried chicken, honey spice sauce<br>9.95</p>
+        </div>
+        <div class="item">
+          <h4>Sesame Chicken</h4>
+          <p>tempura fried chicken, teriyaki sauce<br>9.95</p>
+        </div>
+      </div>
+
+
+      <div class="item-title">Raw Fish</div>
+
+      <div class="items">
+        <div class="item">
+          <h4>Kuma Oyster</h4>
+          <p>half shell oyster with spicy ponzu sauce, lemon - 4 pcs<br>9.95</p>
+        </div>
+        <div class="item">
+          <h4>Pepper Fin</h4>
+          <p>white tuna with jalapeno, spicy ponzu sauce and sesame oil<br>14.95</p>
+        </div>
+        <div class="item">
+          <h4>7 Pcs Sashimi</h4>
+          <p>choice of one - salmon, tuna, yellowtail - 7 pcs<br>14.50</p>
+        </div>
+        <div class="item">
+          <h4>Assorted Sashimi</h4>
+          <p>chef's choice of today's raw fish - 12 pcs<br>19.95</p>
+        </div>
+        <div class="item">
+          <h4>Garlic Sashimi</h4>
+          <p>7pcs of salmon with fried garlic special sauce<br>14.95</p>
+        </div>
+        <div class="item">
+          <h4>Hamachi Carpaccio</h4>
+          <p>Sliced Hamachi jalapeno with chili pepper ponzu olive oil<br>14.95</p>
+        </div>
+      </div>
+    </div>
+
+  <div class="menu-category" id="nigiri">Nigiri</div><hr/>
+  <div class="menu-content" id="nigiri-content">
+
+    <div class="item-title">Nigiri</div>
+    <div class="items">
+      <div class="item">
+          <h4>Albacore</h4>
+          <p>white tuna<br>5.95</p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+      <div class="item">
+        <h4></h4>
+        <p><br></p>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="menu-category" id="street-food">Street Food</div><hr/>
+  <div class="menu-content" id="street-food-content">
+    <p>* 1 order = 2 pieces *</p>
+  </div>
+
+  <div class="menu-category" id="entree">Entrée</div><hr/>
+  <div class="menu-content" id="entree-content">
+    <p>Test Menu 1</p>
+  </div>
+
+  <div class="menu-category" id="kids">Kids' Menu</div><hr/>
+  <div class="menu-content" id="kids-content">
+    <p>Test Menu 1</p>
+  </div>
+
+  <div class="menu-category" id="rolls">Rolls</div><hr/>
+  <div class="menu-content" id="rolls-content">
+    <p>Test Menu 1</p>
+  </div>
+
+  <div class="menu-category" id="ksr">Kuma Special Rolls</div><hr/>
+  <div class="menu-content" id="ksr-content">
+    <p>Test Menu 1</p>
+  </div>
+
+  <div class="menu-category" id="sides">Sides</div><hr/>
+  <div class="menu-content" id="sides-content">
+    <p>Test Menu 1</p>
+  </div>
+
+  <div class="menu-category" id="alcohol">Alcohol Beverage</div><hr/>
+  <div class="menu-content" id="alcohol-content">
+    <p>Test Menu 1</p>
+  </div>
+
+  <div class="menu-category" id="bnd">Beverage & Dessert</div><hr/>
+  <div class="menu-content" id="bnd-content">
+    <h1>Title</h1>
+    <p>Test Menu 1</p>
+
+  </div>
 
 </div>
